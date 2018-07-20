415ac16d2cda13772e7c9b5cd1476febc7138234
liyanguo <lyg@meitu.com>
2018-07-20 18:18:36 +0800

    init project

---
+++ data_structure/queue.js
@@ -0,0 +1,55 @@
+function Queue() {
+    var items = [];
+
+    this.enqueue = function (element) {
+        items.push(element);
+    };
+
+    this.dequeue = function () {
+        return items.shift();
+    };
+
+    this.front = function () {
+        return items[0];
+    };
+
+    this.isEmpty = function () {
+        return items.length === 0;
+    };
+
+    this.clear = function () {
+        items = [];
+    };
+
+    this.size = function () {
+        return items.length;
+    };
+
+    this.print = function () {
+        console.log(items.toString());
+    };
+}
+
+
+function hotPotato(nameList, num) {
+    var queue = new Queue();
+
+    for (var i = 0; i < nameList.length; i++) {
+        queue.enqueue(nameList[i]);
+    }
+
+    var eliminated = '';
+    while (queue.size() > 1) {
+        for (var i = 0; i < num; i++) {
+            queue.enqueue(queue.dequeue());
+        }
+        eliminated = queue.dequeue();
+        console.log(eliminated + '淘汰');
+    }
+
+    return queue.dequeue();
+}
+
+var names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl'];
+var winner = hotPotato(names, 10);
+console.log('Winner: ' + winner);
---
+++ data_structure/stack.js
@@ -0,0 +1,50 @@
+function Stack() {
+    var items = [];
+
+    this.push = function (element) {
+        items.push(element);
+    };
+
+    this.pop = function () {
+        return items.pop();
+    };
+
+    this.peek = function () {
+        return items[items.length - 1];
+    };
+
+    this.isEmpty = function () {
+        return items.length === 0;
+    };
+
+    this.size = function () {
+        return items.length;
+    };
+
+    this.clear = function () {
+        items = [];
+    };
+
+    this.print = function () {
+        console.log(items.toString());
+    }
+}
+
+function baseConvetor(decNumber, base) {
+    var remStack = new Stack();
+    var rem;
+    var baseString = '';
+    var digits = '0123456789ABCDEF';
+
+    while (decNumber > 0) {
+        rem = Math.floor(decNumber % base);
+        remStack.push(rem);
+        decNumber = Math.floor(decNumber / base);
+    }
+
+    while (!remStack.isEmpty()) {
+        baseString += digits[remStack.pop()];
+    }
+
+    return baseString;
+}
