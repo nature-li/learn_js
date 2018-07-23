3da6873f83f7469043ffcdb808107b6705c61bff
liyanguo <lyg@meitu.com>
2018-07-23 18:42:35 +0800

    grapth

--- data_structure/graph.js
+++ data_structure/graph.js
@@ -99,46 +99,135 @@ function Graph() {
             distances: d,
             predecessors: pred
         }
-    }
+    };
+
+    this.dfs = function (callback) {
+        var color = initializeColor();
+        for (var i = 0; i < vertices.length; i++) {
+            if (color[vertices[i]] === 'white') {
+                dfsVisit(vertices[i], color, callback);
+            }
+        }
+    };
+
+    var dfsVisit = function (u, color, callback) {
+        color[u] = 'grey';
+        if (callback) {
+            callback(u);
+        }
+
+        var neighbors = adjList.get(u);
+        for (var i = 0; i < neighbors.length; i++) {
+            var w = neighbors[i];
+            if (color[w] === 'white') {
+                dfsVisit(w, color, callback);
+            }
+        }
+
+        color[u] = 'black';
+    };
+
+    var time = 0;
+    this.DFS = function () {
+        var color = initializeColor();
+        var d = [];
+        var f = [];
+        var p = [];
+        time = 0;
+
+        for (var i = 0; i < vertices.length; i++) {
+            f[vertices[i]] = 0;
+            d[vertices[i]] = 0;
+            p[vertices[i]] = null;
+        }
+
+        for (i = 0; i < vertices.length; i++) {
+            if (color[vertices[i]] === 'white') {
+                DFSVisit(vertices[i], color, d, f, p);
+            }
+        }
+
+        return {
+            discovery: d,
+            finished: f,
+            predecessors: p
+        }
+    };
+
+    var DFSVisit = function (u, color, d, f, p) {
+        console.log('discoverd ' + u);
+        color[u] = 'grey';
+        d[u] = ++time;
+
+        var neighbors = adjList.get(u);
+        for (var i = 0; i < neighbors.length; i++) {
+            var w = neighbors[i];
+            if (color[w] === 'white') {
+                p[w] = u;
+                DFSVisit(w, color, d, f, p);
+            }
+        }
+
+        color[u] = 'black';
+        f[u] = ++time;
+        console.log('expored ' + u);
+    };
 }
 
+// var graph = new Graph();
+// var myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+// for (var i = 0; i < myVertices.length; i++) {
+//     graph.addVertex(myVertices[i]);
+// }
+// graph.addEdge('A', 'B');
+// graph.addEdge('A', 'C');
+// graph.addEdge('A', 'D');
+// graph.addEdge('C', 'D');
+// graph.addEdge('C', 'G');
+// graph.addEdge('D', 'G');
+// graph.addEdge('D', 'H');
+// graph.addEdge('B', 'E');
+// graph.addEdge('B', 'F');
+// graph.addEdge('E', 'I');
+
+// console.log(graph.toString());
+// graph.bfs(myVertices[0], console.log);
+
+
+// var shortestPathA = graph.BFS(myVertices[0]);
+// console.log(shortestPathA);
+
+// var fromVertex = myVertices[0];
+// for (var i = 1; i < myVertices.length; i++) {
+//     var toVertex = myVertices[i];
+//     var path = new Stack();
+//
+//     for (var v = toVertex; v !== fromVertex; v = shortestPathA.predecessors[v]) {
+//         path.push(v);
+//     }
+//     path.push(fromVertex);
+//
+//     var s = path.pop();
+//     while (!path.isEmpty()) {
+//         s += ' -> ' + path.pop();
+//     }
+//     console.log(s);
+// }
+
+
+// graph.dfs(console.log);
+
+
+
 var graph = new Graph();
-var myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+var myVertices = ['A', 'B', 'C', 'D', 'E', 'F'];
 for (var i = 0; i < myVertices.length; i++) {
     graph.addVertex(myVertices[i]);
 }
-graph.addEdge('A', 'B');
 graph.addEdge('A', 'C');
 graph.addEdge('A', 'D');
-graph.addEdge('C', 'D');
-graph.addEdge('C', 'G');
-graph.addEdge('D', 'G');
-graph.addEdge('D', 'H');
+graph.addEdge('B', 'D');
 graph.addEdge('B', 'E');
-graph.addEdge('B', 'F');
-graph.addEdge('E', 'I');
-
-// console.log(graph.toString());
-//
-// graph.bfs(myVertices[0], console.log);
-//
-//
-var shortestPathA = graph.BFS(myVertices[0]);
-// console.log(shortestPathA);
-
-var fromVertex = myVertices[0];
-for (var i = 1; i < myVertices.length; i++) {
-    var toVertex = myVertices[i];
-    var path = new Stack();
-
-    for (var v = toVertex; v !== fromVertex; v = shortestPathA.predecessors[v]) {
-        path.push(v);
-    }
-    path.push(fromVertex);
-
-    var s = path.pop();
-    while (!path.isEmpty()) {
-        s += ' -> ' + path.pop();
-    }
-    console.log(s);
-}
+graph.addEdge('C', 'F');
+graph.addEdge('F', 'E');
+var result = graph.DFS();
