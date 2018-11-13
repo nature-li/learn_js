4fbc360da8bfa974c2817628255fa3640cac4df8
liyanguo <lyg@meitu.com>
2018-11-13 16:14:47 +0800

    add functions

--- base/cross_browser.js
+++ base/cross_browser.js
@@ -103,3 +103,11 @@ function convertToArray(nodes) {
 function isArray(value) {
     return Object.prototype.toString.call(value) === '[object Array]';
 }
+
+function isFunction(value) {
+    return Object.prototype.toString.call(value) === '[object Function]';
+}
+
+function isRegExp(value) {
+    return Object.prototype.toString.call(value) === '[object RegExp]';
+}
