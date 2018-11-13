30fad93c203b3c2911811f9d7aa547d0833cccdf
liyanguo <lyg@meitu.com>
2018-11-13 16:38:03 +0800

    add functions

--- base/cross_browser.js
+++ base/cross_browser.js
@@ -111,3 +111,15 @@ function isFunction(value) {
 function isRegExp(value) {
     return Object.prototype.toString.call(value) === '[object RegExp]';
 }
+
+
+// 作用域安全的构造函数
+function Person(name, age, job) {
+    if (this instanceof Person) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+    } else {
+        return new Person(name, age, job);
+    }
+}
